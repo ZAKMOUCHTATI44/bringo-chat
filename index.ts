6dcfe7d999681c69ca3b7b3a918829e7ed74f44a
Zakaria MOUCHTATI <z.mouchtati@gmail.com>
2025-04-29 18:29:13 +0100

    deploy

--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
 import express, { Request, Response } from "express";
-import cors from "cors";
-require('dotenv').config()
-
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import chatRoutes from "./src/routes/chat"
+dotenv.config();
 const app = express();
-app.use(express.json());
-app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("HEllo world");
-});
+app.use(bodyParser.json());
+app.use("/chat", chatRoutes);
 
-app.listen(4000, () => {
-  console.log("app started");
-});
-export default app;
+app.get("/",(req:Request , res : Response) => {
+  res.send("HElloWORLD")
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Bringo AI bot running on port ${PORT}`));
+
